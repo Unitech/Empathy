160766c9880ff2042f08b3812f493d40acba3e06
root <root@raspberrypi.(none)>
2012-07-20 03:26:23 +0200

    fix

--- test.js
+++ test.js
@@ -9,19 +9,24 @@ var play = require('play');
 var cronJob = require('cron').CronJob;
 
 
-new cronJob('* 10 3 * * *', function(){
+new cronJob('00 28 3 * * *', function(){
     console.log('launched');
-play.sound({
-    file :__dirname + '/two.mp3',
-    volume : 0,
-    soundcard : 'hw:0,0'
-});
 
-play.increaseSlowly(function() {
-    //process.exit(1);
-});
+    play.sound({
+	file :__dirname + '/two.mp3',
+	volume : 0,
+	soundcard : 'hw:0,0'
+    }, function() {
+	play.increaseSlowly(function() {
+	    //process.exit(1);
+	});
+
+    });
+    
     
 }, null, true, "Europe/Paris");
+
+
 
 //
 // Event loop
