2bc623eadc38b85cb0cd0cf1f1f52545f3e6e3ce
strzel-a <strzel-a@ubuntu-home.(none)>
2012-07-20 03:08:08 +0200

    Po

--- test.js
+++ test.js
@@ -1,23 +1,31 @@
-
+//
+// by Alexandre Strzelewicz
+// Mini alarm with progressive sound -- Full of hack (cf node_modules/play/lib/play.js)
+//
+//
 
 
 var play = require('play');
+var cronJob = require('cron').CronJob;
 
+
+new cronJob('* 10 3 * * *', function(){
+    console.log('launched');
 play.sound({
     file :__dirname + '/two.mp3',
     volume : 0,
     soundcard : 'hw:0,0'
 });
 
-// setTimeout(function() {
-//     play.setVolume(0);
-// }, 500);
-
 play.increaseSlowly(function() {
-    process.exit(1);
+    //process.exit(1);
 });
+    
+}, null, true, "Europe/Paris");
 
-
+//
+// Event loop
+//
 (function tmp() {
     setTimeout(function() {
 	process.nextTick(tmp);
